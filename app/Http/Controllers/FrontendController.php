a41bd99a73db204745ab03efb2209ac16c412917
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-24 23:27:06 +0700

    update

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -19,8 +19,6 @@ class FrontendController extends Controller
         $graph = new \EasyRdf\Graph($baseUrl . '/rdfFile/recommend.rdf');
         $graph->load();
         $doc = $graph->primaryTopic();
-
-        dd($graph);
         // //   Deklarasi namespace
         \EasyRdf\RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
         \EasyRdf\RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
