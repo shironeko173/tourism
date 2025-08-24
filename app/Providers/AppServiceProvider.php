c2957ebe35df282b71bc3bad7368927cc593e18e
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-24 22:43:46 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env('APP_ENV') === 'production') {
-        // URL::forceScheme('https');
-        URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
         }
     }
 }
